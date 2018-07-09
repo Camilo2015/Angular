bcb0ecb4d4fbf42d025e65ad73c47cdccaf016f0
Camilo Contreras <crojas@pocketresult.com>
2018-07-09 16:53:13 -0500

    Header and Footer Risto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
@@ -11,44 +11,47 @@ import {
   MatSidenavModule,
   MatFormFieldModule,
   MatInputModule,
-  MatListModule, 
-  MatTooltipModule, 
-  MatCheckboxModule, 
+  MatListModule,
+  MatTooltipModule,
+  MatCheckboxModule,
   MatButtonModule,
   MatButtonToggleModule,
-  MatGridListModule } from '@angular/material'; 
-import { FlexLayoutModule } from '@angular/flex-layout'; 
+  MatGridListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
-import  'hammerjs';
+import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent, 
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     BrowserAnimationsModule,
-    FlexLayoutModule, 
-    FormsModule, 
+    FlexLayoutModule,
+    FormsModule,
     MatMenuModule,
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    MatListModule, 
+    MatListModule,
     MatSidenavModule,
     MatFormFieldModule,
     MatInputModule,
     MatTooltipModule,
     MatButtonModule,
-    MatButtonToggleModule, 
+    MatButtonToggleModule,
     MatCheckboxModule,
     MatGridListModule
-    
   ],
   providers: [ DishService ],
   bootstrap: [AppComponent]
