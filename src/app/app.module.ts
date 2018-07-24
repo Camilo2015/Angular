c31ab562d381f41515de4efcce8d221b9cbb317c
Camilo Contreras <crojas@pocketresult.com>
2018-07-24 18:20:32 -0500

    Angular Router basics :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,10 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -32,7 +36,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +58,8 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatButtonToggleModule,
     MatCheckboxModule,
-    MatGridListModule
+    MatGridListModule,
+    AppRoutingModule
   ],
   providers: [ DishService ],
   bootstrap: [AppComponent]
